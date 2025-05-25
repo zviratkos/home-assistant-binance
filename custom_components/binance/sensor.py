f1495177a7f0c14b47f2674e0f6c0d3ea3beeb9b
Zviratkos <github@stribrsky.cz>
2025-05-25 20:52:48 +0200

    Another update to async mode & warning fix

--- custom_components/binance/sensor.py
+++ custom_components/binance/sensor.py
@@ -4,24 +4,16 @@ import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from .const import (
-    CONF_SYMBOLS,
-    CONF_DECIMALS,
-    CONF_UPDATE_INVERVAL
-)
+from .const import CONF_SYMBOLS, CONF_DECIMALS, CONF_UPDATE_INVERVAL
+from .binance_ticker_sensor import BinanceTickerSensor, BinanceDataCoordinator
 
-from .binance_ticker_sensor import BinanceTickerSensor
+_LOGGER = logging.getLogger(__name__)
 
-
-logger = logging.getLogger(__name__)
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_SYMBOLS): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_DECIMALS, default=8): cv.positive_int,
-        vol.Optional(CONF_UPDATE_INVERVAL, default=60): cv.positive_int
-    }
-)
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_SYMBOLS): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_DECIMALS, default=8): cv.positive_int,
+    vol.Optional(CONF_UPDATE_INVERVAL, default=60): cv.positive_int,
+})
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     symbols = config[CONF_SYMBOLS]
@@ -30,10 +22,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     sensors = []
     for symbol in symbols:
-        logger.debug("Setup BinanceTickerSensor %s %s %s", symbol, decimals, updateInterval)
         coordinator = BinanceDataCoordinator(hass, symbol, interval)
-        await coordinator.async_config_entry_first_refresh()
-        sensors.append(BinanceTickerSensor(symbol, decimals, coordinator))
+        await coordinator.async_refresh()  # instead of async_config_entry_first_refresh()
+        sensors.append(BinanceTickerSensor(symbol, decimals, interval, coordinator))
 
     async_add_entities(sensors, update_before_add=True)
 
